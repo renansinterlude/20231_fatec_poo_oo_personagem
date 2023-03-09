2129c6637af51ab8053ec83aeb27db6f3d39995b
Renan Moreira <renan.batista7@fatec.sp.gov>
2023-03-09 17:52:53 -0300

    apostila02(oo) : bloco de codigo 2.5.1 encerrado

--- Personagem.java
+++ Personagem.java
@@ -2,9 +2,27 @@ import static java.lang.Math.min;
 public class Personagem {
     // variável de instância
     String nome;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
+    private int energia = 10;
+    private int fome = 0;
+    private int sono = 0;
+
+    //construtor padrao: lista de parametros vazia
+    Personagem (int energia, int fome, int sono) {
+        //shadowing. passa variaveis do parametro na frente da outra
+        if(energia >= 10 && energia <= 10)
+            this.energia = energia;
+
+        if(fome >= 0 && fome <= 10)
+            this.fome = fome;
+        
+        if (sono >= 0 && sono <=10)
+            this.sono = sono;
+        
+        this.energia = energia;
+        this.fome = fome;
+        this.sono = sono;
+        System.out.println("Construindo um objeto...");
+    }
 
     //println == print line
     // isso é um método
@@ -20,7 +38,9 @@ public class Personagem {
         fome = min(fome + 1, 10); //-> minimo, incremento, maximo.
         // fome = Math.min se o pacote Math nao tivesse sido importado.
         sono = min(sono + 1, 10);
+        System.out.println(obterEstado());
     } 
+
 
     void comer() {
         if (fome >= 1) { // garante que a fome só seja descrescida se ele TIVER fome.
@@ -31,6 +51,7 @@ public class Personagem {
         else {
             System.out.println(nome + " sem fome");
         }
+        System.out.println(obterEstado());
     }
 
     void dormir() {
@@ -43,5 +64,10 @@ public class Personagem {
         else {
             System.out.println(nome + " sem sono");
         }
+        System.out.println(obterEstado());
+    }
+
+    String obterEstado(){
+        return String.format("e(%d), s(%d), f(%d)", energia, sono, fome);
     }
 }
--- TestePersonagem.java
+++ TestePersonagem.java
@@ -1,14 +1,41 @@
 public class TestePersonagem {
-    public static void main (String [] args) {
+    public static void main (String [] args) throws Exception {
+
         //p é uma variavel de referencia. Ela faz referencia ao objeto construido com new
         // Personagem p = new Personagem ();
         // java 10+ (inferencia de tipo)
         // var = cria uma variavel de referencia. 
-        var p = new Personagem();
+        var p = new Personagem(2, 4, 10); // chamada de construtor
+        var p2 = new Personagem(8, 10, 2);
+        
         p.nome = "John"; // p. -> operador de acesso a membro (de Personagem)
+        // 10, 0, 0, John -> Estado: valores que existem no instante da execução.
+        while (true) {
         p.cacar();
+        Thread.sleep(5000);
         p.comer();
+        Thread.sleep(5000);
         p.dormir();
-
+        Thread.sleep(5000);
+        p.cacar();
+        Thread.sleep(5000);
+        p.cacar();
+        Thread.sleep(5000);
+        p.cacar();
+        System.out.println("=======================================");
+        Thread.sleep(10000);
+        }
     }
+
 }
+
+/*int idade = 18;
+
+        if (idade >= 18) {
+            System.out.println("Pode dirigir");
+        }
+        else {
+            System.out.println("Não pode dirigir");
+        }
+
+        System.out.println(idade >= 18 ? "Sim" : "Nao"); // operador ternario */
