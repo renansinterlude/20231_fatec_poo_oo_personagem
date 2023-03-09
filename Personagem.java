67f4a79550a836b891db9c2c4d5fadb4b2cea5a4
Renan Moreira <renan.batista7@fatec.sp.gov>
2023-03-09 16:25:32 -0300

    apostila02(oo) : bloco de codigo 2.3.1 encerrado.

--- Personagem.java
+++ Personagem.java
@@ -1,21 +1,47 @@
+import static java.lang.Math.min;
 public class Personagem {
     // variável de instância
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
 
     //println == print line
     // isso é um método
     void cacar() {
-        System.out.println(nome + " cacando");
+        if (energia >= 2) {
+           System.out.println(nome + " cacando"); 
+           energia = energia - 2;
+           // energia -= 2
+        }
+        else {
+            System.out.println(nome + " sem energia para cacar.");
+        }
+        fome = min(fome + 1, 10); //-> minimo, incremento, maximo.
+        // fome = Math.min se o pacote Math nao tivesse sido importado.
+        sono = min(sono + 1, 10);
     } 
 
     void comer() {
-        System.out.print(nome + " comendo\n");
+        if (fome >= 1) { // garante que a fome só seja descrescida se ele TIVER fome.
+            System.out.print(nome + " comendo\n"); 
+            energia = min(energia + 1, 10);
+            fome -= 1; // fome = fome -1 ou fome --; 
+        }
+        else {
+            System.out.println(nome + " sem fome");
+        }
     }
 
     void dormir() {
-        System.out.printf("%s dormindo%n", nome);
+        if (sono >= 1) {
+            System.out.printf("%s dormindo%n", nome);
+            energia = energia + 1 < 10 ? energia + 1 : 10; // operador ternario
+            // energia + 1 é menor que 10? se sim, energia +1, se nao energia = 10
+            sono--;
+        }
+        else {
+            System.out.println(nome + " sem sono");
+        }
     }
 }
